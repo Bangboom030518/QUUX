6187fa06a8c14beb530b2a8df74efbe31b5eb6b7
Charlie Clift <bang.boom030518@gmail.com>
2022-11-08 18:58:57 +0000

    Almost finish stores but then references go brrrrr
    Co-authored-by: AlanRandon <AlanRandon@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap, rc::Rc, sync::Mutex};
 
 pub fn escape(input: &str) -> String {
     input
@@ -9,6 +9,10 @@ pub fn escape(input: &str) -> String {
 
 pub type Context = HashMap<String, String>;
 
+pub struct RenderData {
+    pub html: String,
+}
+
 pub trait Component {
     type Props;
 
@@ -18,20 +22,45 @@ pub trait Component {
     fn render(&self, context: Context);
 
     #[cfg(not(target = "wasm"))]
-    fn render(&self, context: Context) -> String;
+    fn render(&self) -> RenderData;
 }
 
-pub struct Store<T: std::fmt::Display> {
+pub struct Store<'a, T: std::fmt::Display>
+where
+    Self: 'a,
+{
     value: T,
+    listeners: Vec<Box<dyn FnMut(&T, &T) + 'a>>,
 }
 
-impl<T: std::fmt::Display> Store<T> {
+impl<'a, T: std::fmt::Display> Store<'a, T> {
     pub fn new(value: T) -> Self {
-        Self { value }
+        Self {
+            value,
+            listeners: Vec::new(),
+        }
+    }
+
+    fn on_change<F>(&mut self, listener: F)
+    where
+        F: FnMut(&T, &T) + 'a,
+    {
+        self.listeners.push(Box::new(listener));
+    }
+
+    fn get(&self) -> &T {
+        &self.value
+    }
+
+    fn set(&mut self, value: T) {
+        for listener in self.listeners.iter_mut() {
+            listener(&self.value, &value);
+        }
+        self.value = value;
     }
 }
 
-impl<T: std::fmt::Display> std::fmt::Display for Store<T> {
+impl<'a, T: std::fmt::Display> std::fmt::Display for Store<'a, T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.value)
     }
@@ -41,4 +70,38 @@ pub fn init_app<T, P>(component: T)
 where
     T: Component<Props = P>,
 {
+}
+
+#[test]
+fn store_test() {
+    let result_1: Mutex<Vec<(u8, u8)>> = Mutex::new(Vec::new());
+    let result_2: Mutex<Vec<(u8, u8)>> = Mutex::new(Vec::new());
+    // let mut a = |&previous, &current| result_1.push((previous, current));
+    // let mut b = |&previous, &current| result_2.push((previous + 10, current + 10));
+    {
+        let mut store = Store::new(0);
+        store.on_change(move |&previous, &current| {
+            (result_1).lock().unwrap().push((previous, current))
+        });
+
+        store.on_change(|&previous, &current| {
+            (result_2)
+                .lock()
+                .unwrap()
+                .push((previous + 10, current + 10))
+        });
+
+        for _ in 0..3 {
+            store.set(store.get() + 1);
+        }
+    }
+
+    assert_eq!(
+        result_1.lock().unwrap().as_slice(),
+        &[(0, 1), (1, 2), (2, 3)]
+    );
+    assert_eq!(
+        result_2.lock().unwrap().as_slice(),
+        &[(10, 11), (11, 12), (12, 13)]
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
-use quux::{Component, init_app, Context, Store};
+use quux::{init_app, Component, Context, RenderData, Store};
 
-struct App {
-    count: Store<u32>,
-}
+// struct App {
+//     count: Store<u32>,
+// }
 
-impl Component for App {
-    type Props = ();
+// impl Component for App {
+//     type Props = ();
 
-    fn init(props: Self::Props) -> Self {
-        Self {
-            count: Store::new(0)
-        }
-    }
+//     fn init(props: Self::Props) -> Self {
+//         Self {
+//             count: Store::new(0)
+//         }
+//     }
 
-    fn render(&self, context: Context) -> String {
-        format!("<button id='random'>{}</button>", self.count)
-    }
-}
+//     fn render(&self) -> RenderData {
+//         format!("<button id='random'>{}</button>", self.count)
+//     }
+// }
 
 fn main() {
-    init_app(App::init(()));
+    let mut my_juicy_store = Store::new(0);
+    my_juicy_store
+        .on_change(|previous, current| println!("{} will change to {}, juicy!", previous, current));
+
+    my_juicy_store.on_change(|_, current| {
+        println!("I found a {}", current);
+    });
+
+    my_juicy_store.on_change(|previous, _| {
+        println!("It was {}", previous);
+    });
+
+    for _ in 0..3 {
+        my_juicy_store.set(my_juicy_store.get() + 1);
+    }
+    // init_app(App::init(()));
 }
