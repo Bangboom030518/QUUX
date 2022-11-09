786bf151a14eae7123f98c3292ba10ba9c855bce
Charlie Clift <bang.boom030518@gmail.com>
2022-11-09 18:29:39 +0000

    Finish store fixes - RefCells go brrr
    Co-authored-by: AlanRandon <AlanRandon@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc, sync::Mutex};
+use std::{cell::RefCell, collections::HashMap, mem::transmute, rc::Rc, sync::Mutex};
 
 pub fn escape(input: &str) -> String {
     input
@@ -25,15 +25,18 @@ pub trait Component {
     fn render(&self) -> RenderData;
 }
 
+pub type StoreCallback<'a, T> = dyn FnMut(&T, &T) + 'a;
+
 pub struct Store<'a, T: std::fmt::Display>
 where
     Self: 'a,
 {
     value: T,
-    listeners: Vec<Box<dyn FnMut(&T, &T) + 'a>>,
+    listeners: Vec<Box<StoreCallback<'a, T>>>,
 }
 
 impl<'a, T: std::fmt::Display> Store<'a, T> {
+    /// Creates a new store.
     pub fn new(value: T) -> Self {
         Self {
             value,
@@ -41,18 +44,23 @@ impl<'a, T: std::fmt::Display> Store<'a, T> {
         }
     }
 
-    fn on_change<F>(&mut self, listener: F)
+    /// Pushes a closure to run whenever the state is changed. The closure will recieve the previous, and new interior value.
+    /// 
+    /// > *NOTE*: You will need to wrap any values borrowed by the closure in a [`RefCell`](https://doc.rust-lang.org/std/cell/struct.RefCell.html) or similar if you plan to use it again afterwards.
+    pub fn on_change<F>(&mut self, listener: F)
     where
         F: FnMut(&T, &T) + 'a,
     {
         self.listeners.push(Box::new(listener));
     }
 
-    fn get(&self) -> &T {
+    /// Returns a reference to the current value
+    pub fn get(&self) -> &T {
         &self.value
     }
-
-    fn set(&mut self, value: T) {
+    
+    /// Sets interior value to `value`.
+    pub fn set(&mut self, value: T) {
         for listener in self.listeners.iter_mut() {
             listener(&self.value, &value);
         }
@@ -74,34 +82,20 @@ where
 
 #[test]
 fn store_test() {
-    let result_1: Mutex<Vec<(u8, u8)>> = Mutex::new(Vec::new());
-    let result_2: Mutex<Vec<(u8, u8)>> = Mutex::new(Vec::new());
-    // let mut a = |&previous, &current| result_1.push((previous, current));
-    // let mut b = |&previous, &current| result_2.push((previous + 10, current + 10));
-    {
-        let mut store = Store::new(0);
-        store.on_change(move |&previous, &current| {
-            (result_1).lock().unwrap().push((previous, current))
-        });
+    let result_1: RefCell<Vec<(u8, u8)>> = RefCell::new(Vec::new());
+    let result_2: RefCell<Vec<(u8, u8)>> = RefCell::new(Vec::new());
+    let mut store = Store::new(0);
+    store.on_change(|&previous, &current| result_1.borrow_mut().push((previous, current)));
 
-        store.on_change(|&previous, &current| {
-            (result_2)
-                .lock()
-                .unwrap()
-                .push((previous + 10, current + 10))
-        });
-
-        for _ in 0..3 {
-            store.set(store.get() + 1);
-        }
+    store.on_change(|&previous, &current| {
+        result_2.borrow_mut().push((previous + 10, current + 10));
+    });
+    for _ in 0..3 {
+        store.set(store.get() + 1);
     }
-
+    assert_eq!(result_1.borrow().as_slice(), &[(0, 1), (1, 2), (2, 3)]);
     assert_eq!(
-        result_1.lock().unwrap().as_slice(),
-        &[(0, 1), (1, 2), (2, 3)]
-    );
-    assert_eq!(
-        result_2.lock().unwrap().as_slice(),
+        result_2.borrow().as_slice(),
         &[(10, 11), (11, 12), (12, 13)]
     );
 }
