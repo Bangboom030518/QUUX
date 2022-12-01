07a1a3facf36eaff98c80e88ff90bdafbac8701e
Charlie Clift <bang.boom030518@gmail.com>
2022-12-01 18:24:56 +0000

    Begin deserialization
    Co-authored-by: AlanRandon <AlanRandon@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ use html::view;
 use serde::{Deserialize, Serialize};
 use shared::{Component, QUUXInitialise, RenderData, Store};
 use wasm_bindgen::prelude::*;
+#[cfg(target_arch = "wasm32")]
+use web_sys::{window};
 
 #[wasm_bindgen]
 extern "C" {
@@ -10,9 +12,13 @@ extern "C" {
     fn log(s: &str);
 }
 
+#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen(start)]
 pub fn init_app() {
-    log("Hello!")
+    let init_script = window().unwrap().document().unwrap().get_element_by_id("__quux_init_script__").expect("`__quux_init_script__` not found");
+    let tree = init_script.get_attribute("data-quux-tree").expect("`__quux_init_script__` doesn't have a tree attached");
+    let tree: shared::RenderContext = postcard::from_bytes(&base64::decode(tree).expect("Failed to decode tree as base64")).expect("Render context tree malformatted");
+    log(tree.id);
 }
 
 #[derive(Serialize, Deserialize)]
