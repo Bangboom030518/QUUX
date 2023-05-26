8558fa73aedcc52f4d2fd52c45e0c9b447337b2b
AlanRandon <alan.randon@outlook.com>
2023-05-26 19:32:47 +0100

    start backend with hyper

--- quuxlet/src/main.rs
+++ quuxlet/src/main.rs
@@ -1,86 +1,103 @@
-#![warn(clippy::pedantic, clippy::nursery)]
-// TODO: remove?
-#![allow(clippy::unused_async)]
-#![cfg(not(target_arch = "wasm32"))]
-use quuxlet::pages::{create, error, Create, Discover, Error, Index, Set};
-use sqlx::{Pool, Sqlite};
-use warp::Filter;
+#![feature(async_fn_in_trait, return_position_impl_trait_in_trait, pattern)]
+use http::{Request, Response, Uri};
+use hyper::{
+    server::conn::AddrStream,
+    service::{make_service_fn, service_fn},
+    Body,
+};
+use std::{convert::Infallible, error::Error, future::Future, net::SocketAddr};
 
-fn with_pool(
-    pool: Pool<Sqlite>,
-) -> impl Filter<Extract = (Pool<Sqlite>,), Error = std::convert::Infallible> + Clone {
-    warp::any().map(move || pool.clone())
+// TODO: connection
+trait Handler<I, O, E: Error> {
+    fn handle(&mut self, input: I) -> impl Future<Output = Result<O, E>> + Send + Sync;
+}
+
+trait Server: Handler<Request<Body>, Response<Body>, Infallible> {
+    // TODO: reduce clones?
+    async fn serve(self, addr: impl Into<SocketAddr>)
+    where
+        Self: Sized + Clone + Send + Sync + 'static,
+    {
+        let server = hyper::Server::bind(&addr.into()).serve(make_service_fn(|_: &AddrStream| {
+            let server = self.clone();
+            async move {
+                // let server = self.clone();
+                Ok::<_, Infallible>(service_fn(move |request| {
+                    let server = server.clone();
+                    async move { server.clone().handle(request).await.map(Into::into) }
+                }))
+            }
+        }));
+
+        server.await.unwrap();
+    }
+}
+
+impl<T: Handler<Request<Body>, Response<Body>, Infallible>> Server for T {}
+
+impl<F, Fut, I, O, E> Handler<I, O, E> for F
+where
+    F: FnMut(I) -> Fut,
+    Fut: Future<Output = Result<O, E>> + Send + Sync,
+    E: Error,
+{
+    fn handle(&mut self, input: I) -> impl Future<Output = Result<O, E>> + Send + Sync {
+        self(input)
+    }
+}
+
+#[derive(Debug, Clone, Copy, thiserror::Error)]
+#[error("pattern failed to match on path")]
+struct PathMatchError;
+
+struct PathSegment<P>(P)
+where
+    for<'a> &'a P: std::str::pattern::Pattern<'a>;
+
+impl<P> Handler<Request<Body>, Request<Body>, PathMatchError> for PathSegment<P>
+where
+    for<'a> &'a P: std::str::pattern::Pattern<'a>,
+    P: Send + Sync,
+{
+    fn handle(
+        &mut self,
+        mut input: Request<Body>,
+    ) -> impl Future<Output = Result<Request<Body>, PathMatchError>> + Send + Sync {
+        async move {
+            let uri = input.uri();
+            let Some(path) = uri.path().strip_prefix(&self.0) else {
+                return Err(PathMatchError);
+            }; //?.parse().unwrap();
+
+            let path_and_query = format!(
+                "{path}{}",
+                uri.query()
+                    .map(|value| format!("?{value}"))
+                    .unwrap_or_default()
+            )
+            .parse()
+            .unwrap();
+
+            let mut parts = uri.clone().into_parts();
+            parts.path_and_query = Some(path_and_query);
+
+            *input.uri_mut() = Uri::from_parts(parts).unwrap();
+            Ok(input)
+        }
+    }
 }
 
 #[tokio::main]
-async fn main() {
-    const PORT: u16 = 3000;
-
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let pool = sqlx::sqlite::SqlitePoolOptions::new()
         .max_connections(5)
         .connect("sqlite://src/database/data.db")
         .await
         .unwrap();
 
-    let index = warp::path::end().and(warp::get()).map(|| Index);
+    (|request| async move { Ok::<_, Infallible>(Response::new(Body::from("Hello World!"))) })
+        .serve(([127, 0, 0, 1], 3000))
+        .await;
 
-    let discover = warp::path!("discover")
-        .and(warp::get())
-        .and(with_pool(pool.clone()))
-        .and_then(|pool| async move { Discover::new(&pool).await.map_err(warp::reject::custom) });
-
-    let set = warp::path!("set" / String)
-        .and(warp::get())
-        .and(with_pool(pool.clone()))
-        .and_then(|id: String, pool| async move {
-            Set::new(&pool, &id).await.map_err(warp::reject::custom)
-        });
-
-    // let not_found = warp::path::full().and_then(|path: FullPath| async move {
-    //     Err::<Infallible, _>(warp::reject::custom(error::NotFound(
-    //         path.as_str().parse().unwrap(),
-    //     )))
-    // });
-
-    println!("listening on http://localhost:{PORT}");
-    let create = warp::path!("create")
-        .and(warp::get())
-        .map(|| Create)
-        .or(warp::path!("create")
-            .and(warp::post())
-            .and(with_pool(pool))
-            .and(warp::body::form::<create::PostData>())
-            .and_then({
-                |pool: sqlx::Pool<sqlx::Sqlite>, data: create::PostData| async move {
-                    println!("{data:?}");
-                    let set = quuxlet::data::Set::create(&pool, &data.name, data.terms)
-                        .await
-                        .map_err(|error| warp::reject::custom(error::Database::from(error)))?;
-
-                    // TODO: `.parse()` is infallible
-                    Ok::<_, warp::Rejection>(warp::redirect(
-                        format!("/set/{}", set.id).parse::<http::Uri>().unwrap(),
-                    ))
-                }
-            }));
-
-    warp::serve(
-        index
-            .or(set)
-            .or(create)
-            .or(discover)
-            .or(warp::path!("dist" / "quuxlet_bg.wasm")
-                .and(warp::filters::fs::file("./dist/quuxlet_bg.wasm"))
-                .with(warp::reply::with::header(
-                    "Content-Type",
-                    "application/wasm",
-                )))
-            // .or(not_found)
-            .recover(|rejection| async move {
-                eprintln!("Error: {rejection:?}");
-                Ok::<Error, std::convert::Infallible>(Error::from(rejection))
-            }),
-    )
-    .run(([127, 0, 0, 1], PORT))
-    .await;
+    Ok(())
 }
